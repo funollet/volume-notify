3f130e72a0a65fa36b300f9e1d0e90755844f1af
Jordi Funollet <funollet@fastmail.fm>
2024-07-09 13:17:06 +0200

    refactor funcs and args

--- main.go
+++ main.go
@@ -7,45 +7,58 @@ import (
 	"os/exec"
 )
 
-func notifySend(args []string) ([]byte, error) {
-	replaceId := "2593"
-	timeout := 1000
+func notifyVolumeOsd(percentage int, muted bool, icon string) ([]byte, error) {
+  // The hint is only shown if percentage is in the 0..100 range.
 
 	defaultArgs := make([]string, 0)
 	defaultArgs = append(defaultArgs,
 		"--app-name", "volume-osd",
-		"--replace-id", replaceId,
+		"--replace-id", "2593",
 		"--transient",
-		"--expire-time", fmt.Sprint(timeout),
+		"--expire-time", fmt.Sprint(1000),
 		"--urgency", "low",
 	)
 
-	allArgs := make([]string, 0)
-	allArgs = append(defaultArgs, args...)
-	allArgs = append(allArgs, " ")
+	args := make([]string, 0)
+	args = append(defaultArgs, args...)
 
-	out, err := exec.Command("notify-send", allArgs...).CombinedOutput()
-	return out, err
+  var argIcon string
+  switch {
+  case icon != "":
+    argIcon = icon
+  case muted:
+    argIcon = "audio-volume-muted"
+  default:
+    argIcon = "audio-volume-medium"
+  }
+	args = append(args, "--icon", argIcon)
+
+  switch {
+  case percentage > 0 && percentage <= 100:
+    args = append(args, "--hint", fmt.Sprintf("int:value:%d", percentage))
+  }
+
+	return notifySend(args, "", "")
 }
 
-func notifySendVolume(percentage int, muted bool) ([]byte, error) {
-	icon := "audio-volume-medium"
-	if muted {
-		icon = "audio-volume-muted"
-	}
+func notifySend(args []string, summary string, body string) ([]byte, error) {
+  switch summary {
+  case "":
+    // notify-send raises an error if it receives an empty body
+	  args = append(args, " ")
+  default:
+	  args = append(args, summary)
+  }
 
-	args := make([]string, 0)
-	args = append(args,
-		"--hint", fmt.Sprintf("int:value:%d", percentage),
-		"--icon", icon,
-	)
-	out, err := notifySend(args)
+  args = append(args, body)
+
+	out, err := exec.Command("notify-send", args...).CombinedOutput()
 	return out, err
 }
 
 func main() {
-	muted := true
-	out, err := notifySendVolume(50, muted)
+	// out, err := notifyVolumeOsd(50, false, "")
+	out, err := notifyVolumeOsd(-1, true, "")
 	if err != nil {
 		log.Println(string(out))
 		os.Exit(1)
