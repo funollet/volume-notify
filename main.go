4a10f3eb784d66084e3f4ba26792c3fe919fe7e7
Jordi Funollet <funollet@fastmail.fm>
2024-07-09 22:34:57 +0200

    notifyVolumeOsd() without var args

--- main.go
+++ main.go
@@ -14,84 +14,71 @@ func run(cmd string) (out []byte, err error) {
 	if err != nil {
 		log.Println(string(out))
 	}
-  return out, err
+	return out, err
 }
 
 func getVolume() (volume int, err error) {
 	// Get the current volume
 	var cmdOut []byte
-  cmdOut, err = run("pactl get-sink-volume @DEFAULT_SINK@")
+	cmdOut, err = run("pactl get-sink-volume @DEFAULT_SINK@")
 	if err != nil {
 		return 0, err
 	}
-  parsed := strings.Split(string(cmdOut), "\n")[0]
-  parsedd := strings.Split(parsed, "/")[1]
-  parseddd := strings.ReplaceAll(parsedd, "%", "")
+	parsed := strings.Split(string(cmdOut), "\n")[0]
+	parsedd := strings.Split(parsed, "/")[1]
+	parseddd := strings.ReplaceAll(parsedd, "%", "")
 	return strconv.Atoi(strings.TrimSpace(parseddd))
 }
 
 func notifyVolumeOsd(percentage int, muted bool, icon string) (out []byte, err error) {
-	// The hint is only shown if percentage is in the 0..100 range.
+	timeout := 1000
 
-	defaultArgs := make([]string, 0)
-	defaultArgs = append(defaultArgs,
-		"--app-name", "volume-osd",
-		"--replace-id", "2593",
-		"--transient",
-		"--expire-time", fmt.Sprint(1000), // ms
-		"--urgency", "low",
-	)
+	var iconWithDefaults string
 
-	args := make([]string, 0)
-	args = append(defaultArgs, args...)
-
-	var argIcon string
 	switch {
 	case icon != "":
-		argIcon = icon
+		iconWithDefaults = icon
 	case muted:
-		argIcon = "audio-volume-muted"
+		iconWithDefaults = "audio-volume-muted"
 	default:
-		argIcon = "audio-volume-medium"
+		iconWithDefaults = "audio-volume-medium"
 	}
-	args = append(args, "--icon", argIcon)
 
+	cmd := fmt.Sprintf(`notify-send \
+    --app-name volume-osd \
+    --replace-id 2593 \
+    --transient \
+    --expire-time %d \
+    --urgency low \
+    --icon %s \
+  `, timeout, iconWithDefaults)
+
+	// The hint is only shown if percentage is in the 0..100 range.
 	switch {
 	case percentage > 0 && percentage <= 100:
-		args = append(args, "--hint", fmt.Sprintf("int:value:%d", percentage))
+		cmd += fmt.Sprintf("  --hint int:value:%d \\\n", percentage)
 	}
 
-	return notifySend(args, "", "")
-}
+	// notify-send raises an error if it receives an empty body
+	cmd += "    ' '"
 
-func notifySend(args []string, summary string, body string) (out []byte, err error) {
-	switch summary {
-	case "":
-		// notify-send raises an error if it receives an empty body
-		args = append(args, " ")
-	default:
-		args = append(args, summary)
-	}
-
-	args = append(args, body)
-
-	return exec.Command("notify-send", args...).CombinedOutput()
+	fmt.Println(cmd)
+	return run(cmd)
 }
 
 func main() {
-	// out, err := notifyVolumeOsd(50, false, "")
-	// // out, err := notifyVolumeOsd(-1, true, "")
-	// if err != nil {
-	// 	log.Println(string(out))
-	// 	os.Exit(1)
-	// }
-
-	volume, err := getVolume()
+	out, err := notifyVolumeOsd(50, false, "")
+	// out, err := notifyVolumeOsd(-1, true, "")
 	if err != nil {
+		log.Println(string(out))
 		os.Exit(1)
 	}
-	fmt.Println(volume)
 
+	// volume, err := getVolume()
+	// if err != nil {
+	// 	os.Exit(1)
+	// }
+	// fmt.Println(volume)
 
 	fmt.Println()
 }
