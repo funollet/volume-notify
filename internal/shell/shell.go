8c04d77531e935ae8f6ac3f812596c57060a7e58
Jordi Funollet <funollet@fastmail.fm>
2024-07-27 00:02:32 +0200

    simplify shell functions

--- internal/shell/shell.go
+++ internal/shell/shell.go
@@ -3,7 +3,6 @@ package shell
 import (
 	"log"
 	"os/exec"
-	"strconv"
 )
 
 func Run(cmd string) (result string, err error) {
@@ -13,29 +12,4 @@ func Run(cmd string) (result string, err error) {
 		log.Println(string(out))
 	}
 	return string(out), err
-}
-
-type Shell struct {
-	Command string
-	Process func(string) (string, error)
-}
-
-func (sh Shell) Do() (result string, err error) {
-	out, err := Run(sh.Command)
-	if err != nil {
-		return "", err
-	}
-	return sh.Process(out)
-}
-
-func (sh Shell) DoInt() (result int, err error) {
-	cmdOut, err := Run(sh.Command)
-	if err != nil {
-		return 0, err
-	}
-	out, err := sh.Process(cmdOut)
-	if err != nil {
-		return 0, err
-	}
-	return strconv.Atoi(out)
 }
--- internal/volume/volume.go
+++ internal/volume/volume.go
@@ -3,41 +3,37 @@ package volume
 import (
 	"errors"
 	"funollet/volume-notify/internal/shell"
+	"strconv"
 	"strings"
 )
 
-func processGetVolume(text string) (volume string, err error) {
+func processGetVolume(text string) (volume int, err error) {
 	lines := strings.Split(strings.TrimSpace(text), "\n")
 	firstLine := lines[0]
 	fields := strings.Split(firstLine, "/")
 	if len(fields) < 2 {
-		return "", errors.New("invalid commmand output")
+		return 0, errors.New("invalid commmand output")
 	}
 	value := strings.ReplaceAll(fields[1], "%", "")
-	return strings.TrimSpace(value), nil
+	return strconv.Atoi(strings.TrimSpace(value))
 }
 
 func GetVolume() (volume int, err error) {
-	runner := shell.Shell{
-		Command: "pactl get-sink-volume @DEFAULT_SINK@",
-		Process: processGetVolume,
+	out, err := shell.Run("pactl get-sink-volume @DEFAULT_SINK@")
+	if err != nil {
+		return 0, errors.New("unable to determine volume")
 	}
-	return runner.DoInt()
+	return processGetVolume(out)
 }
 
-func processGetMute(text string) (out string, err error) {
-	return text, nil
+func processGetMute(s string) (bool, error) {
+	return s == "Mute: yes\n", nil
 }
 
 func GetMute() (muted bool, err error) {
-	runner := shell.Shell{
-		Command: "pactl get-sink-mute @DEFAULT_SINK@",
-		Process: processGetMute,
-	}
-	var result string
-	result, err = runner.Do()
+	out, err := shell.Run("pactl get-sink-mute @DEFAULT_SINK@")
 	if err != nil {
-		return false, err
+		return false, errors.New("unable to determine muted status")
 	}
-	return result == "Mute: yes\n", nil
+	return processGetMute(out)
 }
