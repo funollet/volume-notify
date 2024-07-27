cb660cba27c1a6b23a4dedf226471c9bdcd2eafd
Jordi Funollet <funollet@fastmail.fm>
2024-07-27 17:11:26 +0200

    replace flag with pflag

--- main.go
+++ main.go
@@ -1,35 +1,57 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"funollet/volume-notify/internal/notify"
 	"funollet/volume-notify/internal/volume"
+	"github.com/spf13/pflag"
 	"log"
 	"os"
 )
 
 func main() {
-	var step int
-	flag.IntVar(&step, "step", 1, "volume change step")
-	flag.Parse()
-	if len(flag.Args()) < 1 {
+	usage_header := `Changes volume up/down/muted and sends a desktop notification.
+
+Usage: volume-notify [OPTION]... <up|down|mute>
+
+Arguments:
+  up      Turn volume up.
+  down    Turn volume down.
+  mute    Toggle mute status.
+
+Options:
+`
+	pflag.Usage = func() {
+		fmt.Fprintf(os.Stderr, usage_header)
+		pflag.PrintDefaults()
+	}
+	step := pflag.IntP("step", "s", 1, "volume change step, percentual")
+	help := pflag.BoolP("help", "h", false, "show this message")
+
+	pflag.Parse()
+
+	if *help {
+		pflag.Usage()
+		os.Exit(0)
+	}
+
+	if pflag.NArg() < 1 {
 		log.Println("please provide a value")
 		os.Exit(1)
 	}
+	action := pflag.Arg(0)
 
-	switch flag.Arg(0) {
+	switch action {
 	case "up":
-		change := fmt.Sprintf("+%d%%", step)
-		log.Println(change)
-		errSetVolume := volume.SetVolume(change)
-		if errSetVolume != nil {
+		change := fmt.Sprintf("+%d%%", *step)
+		err := volume.SetVolume(change)
+		if err != nil {
 			os.Exit(1)
 		}
 	case "down":
-		change := fmt.Sprintf("-%d%%", step)
-		errSetVolume := volume.SetVolume(change)
-		if errSetVolume != nil {
+		change := fmt.Sprintf("-%d%%", *step)
+		err := volume.SetVolume(change)
+		if err != nil {
 			os.Exit(1)
 		}
 	case "mute":
